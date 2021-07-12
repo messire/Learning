82966033dec4d8a9057fba0dfd2878a75cba9d00
EugeneSmirnov <evgeniy.smirnov@primetals.com>
2021-07-12 14:11:03 +0500

    extract checking substring

--- LongestPalindrome/Program.cs
+++ LongestPalindrome/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.ComponentModel.Design;
 using System.Text;
 
 namespace LongestPalindrome
@@ -50,30 +51,35 @@ namespace LongestPalindrome
 
             for (int i = 0; i < s.Length; i++)
             {
-                StringBuilder odd = new StringBuilder().Append(s[i]);
-                StringBuilder even = new StringBuilder();
-                int step = 1;
+                string odd = CheckSubstring(s, i, 1);
+                string even = CheckSubstring(s, i, 0);
 
-                while (i - step >= 0 && i + step < s.Length && s[i - step] == s[i + step])
-                {
-                    odd.Insert(0, s[i - step]);
-                    odd.Append(s[i + step]);
-                    step++;
-                }
-
-                step = 0;
-                while (i - step >= 0 && i + step + 1 < s.Length && s[i - step] == s[i + step + 1])
-                {
-                    even.Insert(0, s[i - step]);
-                    even.Append(s[i + step + 1]);
-                    step++;
-                }
-
-                result = odd.Length > result.Length ? odd.ToString() : result;
-                result = even.Length > result.Length ? even.ToString() : result;
+                result = odd.Length > result.Length ? odd : result;
+                result = even.Length > result.Length ? even : result;
             }
 
             return result;
+        }
+
+        private static string CheckSubstring(string s, int i, int start)
+        {
+            StringBuilder result = new StringBuilder();
+            if (start == 1)
+            {
+                result.Append(s[i]);
+            }
+
+            int step = start;
+            int k = (start + 1) % 2;
+
+            while (i - step >= 0 && i + step + k < s.Length && s[i - step] == s[i + step + k])
+            {
+                result.Insert(0, s[i - step]);
+                result.Append(s[i + step + k]);
+                step++;
+            }
+
+            return result.ToString();
         }
     }
 }
