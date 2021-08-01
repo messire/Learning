fa5890efeea6b0bb3df0de3a041d621b59253c94
Хаген Смирнов <messirekhagen@live.com>
2021-08-01 20:04:46 +0500

    ZigZagConversion

---
+++ LeetCode.Test/Tests/ZigZagConversionTests.cs
@@ -0,0 +1,67 @@
+﻿using NUnit.Framework;
+
+namespace LeetCode.Test.Tests
+{
+    public class ZigZagConversionTests: BaseTest<ZigZagConversion>
+    {
+        private void CheckResult(string actual, string expected)
+        {
+            Assert.AreEqual(expected, actual);
+        }
+
+        [Test]
+        public void ZigZagConversion_Test1()
+        {
+            string s = "PAYPALISHIRING";
+            string target = "PAHNAPLSIIGYIR";
+            int numRows = 3;
+            string result = Tester.Calculate(s, numRows);
+
+            CheckResult(result, target);
+        }
+        
+        [Test]
+        public void ZigZagConversion_Test2()
+        {
+            string s = "PAYPALISHIRING";
+            string target = "PINALSIGYAHRPI";
+            int numRows = 4;
+            string result = Tester.Calculate(s, numRows);
+
+            CheckResult(result, target);
+        }
+        
+        [Test]
+        public void ZigZagConversion_Test3()
+        {
+            string s = "A";
+            string target = "A";
+            int numRows = 1;
+            string result = Tester.Calculate(s, numRows);
+
+            CheckResult(result, target);
+        }
+        
+        [Test]
+        public void ZigZagConversion_Test4()
+        {
+            string s = "AB";
+            string target = "AB";
+            int numRows = 1;
+            string result = Tester.Calculate(s, numRows);
+
+            CheckResult(result, target);
+        }
+        
+        [Test]
+        public void ZigZagConversion_Test5()
+        {
+            string s = "A";
+            string target = "A";
+            int numRows = 2;
+            string result = Tester.Calculate(s, numRows);
+
+            CheckResult(result, target);
+        }
+    }
+}
---
+++ LeetCode/ZigZagConversion.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Text;
+
+namespace LeetCode
+{
+    // The string "PAYPALISHIRING" is written in a zigzag pattern on a given number of rows like this:
+    // (you may want to display this pattern in a fixed font for better legibility)
+
+    // P   A   H   N
+    // A P L S I I G
+    // Y   I   R
+    
+    // And then read line by line: "PAHNAPLSIIGYIR"
+    // Write the code that will take a string and make this conversion given a number of rows:
+    // string convert(string s, int numRows);
+
+    public class ZigZagConversion
+    {
+        public string Calculate(string s, int numRows)
+        {
+            numRows = Math.Min(s.Length, numRows);
+            StringBuilder[] arr = ConvertToArray(s, numRows);
+            string result = ConvertToString(arr, numRows);
+
+            return result;
+        }
+
+
+        private StringBuilder[] ConvertToArray(string s, int numRows)
+        {
+            StringBuilder[] result = new StringBuilder[numRows];
+
+            bool rev = false;
+            int i = 0;
+
+            foreach (char t in s)
+            {
+                if (result[i] == null)
+                {
+                    result[i] = new StringBuilder();
+                }
+
+                result[i].Append(t);
+                if (numRows == 1) continue;
+                rev = i == numRows - 1 || i != 0 && rev;
+                i += rev ? -1 : 1;
+            }
+
+            return result;
+        }
+
+        private string ConvertToString(StringBuilder[] array, int numRows)
+        {
+            string result = string.Empty;
+            for (int i = 0; i < numRows; i++)
+            {
+                result += array[i].ToString();
+            }
+
+            return result;
+        }
+    }
+}
