353f4ad439937bcdd0409441d3c15429ba783a2c
EugeneSmirnov <evgeniy.smirnov@primetals.com>
2021-08-05 11:36:37 +0500

    MaxArea.cs

---
+++ LeetCode.Test/Tests/MaxAreaTests.cs
@@ -0,0 +1,66 @@
+﻿using NUnit.Framework;
+
+namespace LeetCode.Test.Tests
+{
+    public class MaxAreaTests : BaseTest<MaxArea>
+    {
+        private void CheckResult(int actual, int expected)
+        {
+            Assert.AreEqual(expected, actual);
+        }
+
+        [Test]
+        public void ReverseInteger_Test1()
+        {
+            int[] height = {1, 8, 6, 2, 5, 4, 8, 3, 7};
+            int expected = 49;
+            int result = Tester.Calculate(height);
+            CheckResult(result, expected);
+        }
+
+        [Test]
+        public void ReverseInteger_Test2()
+        {
+            int[] height = {1, 1};
+            int expected = 1;
+            int result = Tester.Calculate(height);
+            CheckResult(result, expected);
+        }
+
+        [Test]
+        public void ReverseInteger_Test3()
+        {
+            int[] height = {4, 3, 2, 1, 4};
+            int expected = 16;
+            int result = Tester.Calculate(height);
+            CheckResult(result, expected);
+        }
+        
+        [Test]
+        public void ReverseInteger_Test4()
+        {
+            int[] height = {1, 2, 1};
+            int expected = 2;
+            int result = Tester.Calculate(height);
+            CheckResult(result, expected);
+        }
+        
+        [Test]
+        public void ReverseInteger_Test5()
+        {
+            int[] height = BigCollections.MaxAreaCollection;
+            int expected = 705634720;
+            int result = Tester.Calculate(height);
+            CheckResult(result, expected);
+        }
+        
+        [Test]
+        public void ReverseInteger_Test6()
+        {
+            int[] height = { 3, 2, 5, 8, 1, 6, 7, 4, 9, 3, 2, 6 };
+            int expected = 48;
+            int result = Tester.Calculate(height);
+            CheckResult(result, expected);
+        }
+    }
+}
---
+++ LeetCode/MaxArea.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+
+namespace LeetCode
+{
+    public class Vector3
+    {
+        public int X { get; set; }
+        public int Y { get; set; }
+        public int Z { get; set; }
+
+        public Vector3(int x, int y, int z)
+        {
+            X = x;
+            Y = y;
+            Z = z;
+        }
+
+        public override string ToString()
+        {
+            return $"X: {X}, Y: {Y}, Z: {Z}";
+        }
+    }
+
+    public class MaxArea
+    {
+        public int Calculate(int[] height)
+        {
+            int result = 0;
+            int i = 0;
+            int j = height.Length-1;
+            
+            while (i < j)
+            {
+                int length = j - i;
+                
+                int width = Math.Min(height[i], height[j]);
+                int s = length * width;
+
+                result = Math.Max(s, result);
+                
+                if (height[i] > height[j])
+                {
+                    j--;
+                }
+                else
+                {
+                    i++;
+                }
+            }
+            
+
+            return result;
+        }
+    }
+}
