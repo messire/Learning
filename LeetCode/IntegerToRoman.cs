9fd05977dabac4f500c8a25cfb91c30f9e5936a7
EugeneSmirnov <evgeniy.smirnov@primetals.com>
2021-08-06 11:34:49 +0500

    add RomanToInteger.cs

--- LeetCode/IntegerToRoman.cs
+++ LeetCode/IntegerToRoman.cs
@@ -34,31 +34,32 @@ namespace LeetCode
         public string Calculate(int num)
         {
             string result = "";
-            Dictionary<int, string> dict = new Dictionary<int, string>() {
-                {1000, "M"},
-                {900, "CM"},
-                {500, "D"},
-                {400, "CD"},
-                {100, "C"},
-                {90, "XC"},
-                {50, "L"},
-                {40, "XL"},
-                {10, "X"},
-                {9, "IX"},
-                {5, "V"},
-                {4, "IV"},
-                {1, "I"},
+            Dictionary<int, string> dict = new Dictionary<int, string>()
+            {
+                { 1000, "M" },
+                { 900, "CM" },
+                { 500, "D" },
+                { 400, "CD" },
+                { 100, "C" },
+                { 90, "XC" },
+                { 50, "L" },
+                { 40, "XL" },
+                { 10, "X" },
+                { 9, "IX" },
+                { 5, "V" },
+                { 4, "IV" },
+                { 1, "I" },
             };
-            
+
             foreach (int key in dict.Keys)
             {
-                while (num>=key)
+                while (num >= key)
                 {
                     num -= key;
                     result += dict[key];
                 }
             }
-            
+
             return result;
         }
     }
