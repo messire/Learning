4e98f55900ca60fdb2ae93e4c50ac76d85e188ec
EugeneSmirnov <evgeniy.smirnov@primetals.com>
2021-08-06 11:07:58 +0500

    small rework IntToRoman

--- LeetCode/IntegerToRoman.cs
+++ LeetCode/IntegerToRoman.cs
@@ -35,46 +35,28 @@ namespace LeetCode
         {
             string result = "";
             Dictionary<int, string> dict = new Dictionary<int, string>() {
-                {0, ""},
-                {1, "I"},
-                {2, "II"},
-                {3, "III"},
-                {4, "IV"},
-                {5, "V"},
-                {6, "VI"},
-                {7, "VII"},
-                {8, "VIII"},
-                {9, "IX"},
-                {10, "X"},
-                {20, "XX"},
-                {30, "XXX"},
-                {40, "XL"},
-                {50, "L"},
-                {60, "LX"},
-                {70, "LXX"},
-                {80, "LXXX"},
-                {90, "XC"},
-                {100, "C"},
-                {200, "CC"},
-                {300, "CCC"},
-                {400, "CD"},
-                {500, "D"},
-                {600, "DC"},
-                {700, "DCC"},
-                {800, "DCCC"},
-                {900, "CM"},
                 {1000, "M"},
-                {2000, "MM"},
-                {3000, "MMM"},
+                {900, "CM"},
+                {500, "D"},
+                {400, "CD"},
+                {100, "C"},
+                {90, "XC"},
+                {50, "L"},
+                {40, "XL"},
+                {10, "X"},
+                {9, "IX"},
+                {5, "V"},
+                {4, "IV"},
+                {1, "I"},
             };
-
-            int[] multipliers = new[] { 1000, 100, 10, 1 };
-
-            foreach (int multiplier in multipliers)
+            
+            foreach (int key in dict.Keys)
             {
-                int amount = num - num % multiplier;
-                result += dict[amount];
-                num -= amount; 
+                while (num>=key)
+                {
+                    num -= key;
+                    result += dict[key];
+                }
             }
             
             return result;
