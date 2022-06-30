62a05a63b14d2c5fcb0035910b5648d1739439ff
Eugene Smirnov <e.smirnov@siridar.com>
2022-06-30 19:53:20 +0500

    added python project; added TwoSum.py added SubListsOfList.py

---
+++ PyLeetCode/TwoSum.py
@@ -0,0 +1,23 @@
+# Given an array of integers nums and an integer target, return indices of the two numbers such that they add up to target.
+# You may assume that each input would have exactly one solution, and you may not use the same element twice.
+# You can return the answer in any order.
+def twoSum(nums: list[int], target: int) -> list[int]:
+    pass
+            
+    return []
+
+
+nums = [2, 7, 11, 15]
+target = 9
+print(twoSum(nums, target))
+# Output: [0,1]
+
+nums = [3, 2, 4]
+target = 6
+print(twoSum(nums, target))
+# Output: [1,2]
+
+nums = [3, 3]
+target = 6
+print(twoSum(nums, target))
+Output: [0, 1]
